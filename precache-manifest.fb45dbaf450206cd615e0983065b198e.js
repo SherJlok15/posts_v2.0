fb4701d27aca4e1f2960ee82b43afcf211b0137b
Fandorin <SherJlok15@gmail.com>
2019-08-19 16:58:13 +0300

    Updates

---
+++ precache-manifest.fb45dbaf450206cd615e0983065b198e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "806f530d630e566fbde2ab7ea9a8b06f",
+    "url": "/posts_v2.0/index.html"
+  },
+  {
+    "revision": "5d7077070fb0d1747acf",
+    "url": "/posts_v2.0/static/css/main.4e650055.chunk.css"
+  },
+  {
+    "revision": "5999ea19a348ef839ef3",
+    "url": "/posts_v2.0/static/js/2.c38ab16a.chunk.js"
+  },
+  {
+    "revision": "5d7077070fb0d1747acf",
+    "url": "/posts_v2.0/static/js/main.a8eb57ff.chunk.js"
+  },
+  {
+    "revision": "edfe0f70d4fc45b1d9fc",
+    "url": "/posts_v2.0/static/js/runtime~main.296999f2.js"
+  }
+]);
